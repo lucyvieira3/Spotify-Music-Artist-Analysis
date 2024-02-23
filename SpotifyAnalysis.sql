170840d780ef3fababd5c051086bf3955af08c6f
Lucia P Vieira <153330654+lucyvieira3@users.noreply.github.com>
2024-02-23 14:10:08 -0500

    Add files via upload

---
+++ SpotifyAnalysis.sql
@@ -0,0 +1,62 @@
+Select *
+From taylor_swift_spotify tss 
+
+Select name, album, release_date, popularity 
+From taylor_swift_spotify tss 
+Order by popularity DESC 
+
+Select name, album, release_date, popularity 
+From taylor_swift_spotify tss 
+Where album like '%version%'
+Order by popularity DESC 
+
+Select name, album, release_date, popularity 
+From taylor_swift_spotify tss 
+Where album like '%fearless%'
+Order by popularity DESC 
+
+Select name, album, release_date, popularity 
+From taylor_swift_spotify tss 
+Where album like '%red%'
+Order by popularity DESC 
+
+Select name, album, release_date, popularity 
+From taylor_swift_spotify tss 
+Where album like '%speak now%'
+Order by popularity DESC 
+
+Select name, album, release_date, popularity 
+From taylor_swift_spotify tss 
+Where album like '%1989%'
+Order by popularity DESC 
+
+Select name, album, release_date, acousticness
+From taylor_swift_spotify tss 
+Order by acousticness DESC 
+
+Select name, album, release_date, acousticness 
+From taylor_swift_spotify tss 
+Where album like '%version%'
+Order by acousticness  DESC 
+
+Select name, album, release_date, acousticness 
+From taylor_swift_spotify tss 
+Where album like '%fearless%'
+Order by acousticness  DESC 
+
+Select name, album, release_date, acousticness 
+From taylor_swift_spotify tss 
+Where album like '%red%'
+Order by acousticness  DESC 
+
+Select name, album, release_date, acousticness 
+From taylor_swift_spotify tss 
+Where album like '%speak now%'
+Order by acousticness  DESC 
+
+Select name, album, release_date, acousticness 
+From taylor_swift_spotify tss 
+Where album like '%1989%'
+Order by acousticness  DESC 
+
+
